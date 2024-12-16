9096fb492920e2af5d84ead2b8dd087d031498c9
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-17 03:48:07 +0530

    this is my video call 2

---
+++ src/context/SocketContext.tsx
@@ -0,0 +1,11 @@
+import SocketIoClient from "socket.io-client";
+import { createContext } from "react";
+const WS_Server="http://localhost:5500";
+const SocketContext=createContext <unknown | null>(null);
+const socket=SocketIoClient(WS_Server);
+interface Props{
+    children:React.ReactNode
+}
+export const SocketProvider: React.FC<Props>=({children})=>{
+    return (<SocketContext.Provider value={{socket}}>{children}</SocketContext.Provider>)
+}
