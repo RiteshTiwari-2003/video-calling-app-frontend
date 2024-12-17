f74eded3521419926adf69836710bcbb04cbb6c8
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-18 03:05:39 +0530

    this is my video call 9 commit

--- src/Components/CreateRoom.tsx
+++ src/Components/CreateRoom.tsx
@@ -1,5 +1,14 @@
-const createRoom: React.FC=()=>{
+import { useContext } from "react";
+import { SocketContext } from "../context/SocketContext";
+
+
+const CreateRoom: React.FC=()=>{
+    const {socket}=useContext(SocketContext);
+    const initRoom=()=>{
+        socket.emit("create-room");
+    }
     return (
-        <button>Start aq new meeting in a new room </button>
+        <button onClick={initRoom} className="btn btn-secondary">Start aq new meeting in a new room </button>
     )
 }
+export default CreateRoom;
--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,7 +1,8 @@
 import SocketIoClient from "socket.io-client";
 import { createContext } from "react";
 const WS_Server="http://localhost:5500";
-const SocketContext=createContext <unknown | null>(null);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const SocketContext=createContext <any | null>(null);
 const socket=SocketIoClient(WS_Server);
 interface Props{
     children:React.ReactNode
