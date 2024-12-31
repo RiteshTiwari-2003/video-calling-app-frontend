a26fbd5f80925a03bc58df6e20b60a755b5c3d0d
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-01 00:01:02 +0530

    this is my dashboard commit

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -1,7 +1,16 @@
+import { useContext,useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { SocketContext } from "../context/SocketContext";
 
 const Room:React.FC=()=>{
     const {id}=useParams();
+    const {socket}=useContext(SocketContext)
+    useEffect(()=>{
+        //emitting this event so that either creator of the room or joinee in the room 
+        //anyone is added the server knownsthat new people have been added 
+        //to this room
+        socket.emit("joined-room",{roomId:id});
+    },[]);
     return (
         <div>
             room:{id}
